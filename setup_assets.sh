b5777b54aef9ca2a6c2aade7ea63ed317336baee
AiStyl <ai.style@outlook.com>
2026-02-24 10:33:02 -0800

    fix polyhaven URLs

--- setup_assets.sh
+++ setup_assets.sh
@@ -7,26 +7,26 @@ mkdir -p $ASSETS_DIR/textures/cardboard
 mkdir -p $ASSETS_DIR/textures/metal_plate
 mkdir -p $ASSETS_DIR/hdri
 
-BASE="https://dl.polyhaven.org/file/ph-assets"
+BASE="https://dl.polyhaven.org/file/ph-assets/Textures/jpg/2k"
 
-echo "Downloading concrete floor texture..."
-curl -L "$BASE/Textures/concrete_floor_02/2k/concrete_floor_02_diff_2k.jpg" -o $ASSETS_DIR/textures/concrete/diff.jpg
-curl -L "$BASE/Textures/concrete_floor_02/2k/concrete_floor_02_rough_2k.jpg" -o $ASSETS_DIR/textures/concrete/rough.jpg
-curl -L "$BASE/Textures/concrete_floor_02/2k/concrete_floor_02_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/concrete/normal.jpg
+echo "Downloading concrete floor textures..."
+curl -L "$BASE/concrete_floor_02/concrete_floor_02_diff_2k.jpg" -o $ASSETS_DIR/textures/concrete/diff.jpg
+curl -L "$BASE/concrete_floor_02/concrete_floor_02_rough_2k.jpg" -o $ASSETS_DIR/textures/concrete/rough.jpg
+curl -L "$BASE/concrete_floor_02/concrete_floor_02_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/concrete/normal.jpg
 
-echo "Downloading wood planks texture..."
-curl -L "$BASE/Textures/wood_planks_dirt/2k/wood_planks_dirt_diff_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/diff.jpg
-curl -L "$BASE/Textures/wood_planks_dirt/2k/wood_planks_dirt_rough_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/rough.jpg
-curl -L "$BASE/Textures/wood_planks_dirt/2k/wood_planks_dirt_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/normal.jpg
+echo "Downloading wood planks textures..."
+curl -L "$BASE/wood_planks_dirt/wood_planks_dirt_diff_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/diff.jpg
+curl -L "$BASE/wood_planks_dirt/wood_planks_dirt_rough_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/rough.jpg
+curl -L "$BASE/wood_planks_dirt/wood_planks_dirt_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/wood_planks/normal.jpg
 
-echo "Downloading metal plate texture..."
-curl -L "$BASE/Textures/metal_plate/2k/metal_plate_diff_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/diff.jpg
-curl -L "$BASE/Textures/metal_plate/2k/metal_plate_rough_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/rough.jpg
-curl -L "$BASE/Textures/metal_plate/2k/metal_plate_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/normal.jpg
-curl -L "$BASE/Textures/metal_plate/2k/metal_plate_metal_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/metal.jpg
+echo "Downloading metal plate textures..."
+curl -L "$BASE/metal_plate/metal_plate_diff_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/diff.jpg
+curl -L "$BASE/metal_plate/metal_plate_rough_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/rough.jpg
+curl -L "$BASE/metal_plate/metal_plate_nor_gl_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/normal.jpg
+curl -L "$BASE/metal_plate/metal_plate_metal_2k.jpg" -o $ASSETS_DIR/textures/metal_plate/metal.jpg
 
-echo "Downloading warehouse HDRI..."
-curl -L "$BASE/HDRIs/industrial_sunset_puresky/2k/industrial_sunset_puresky_2k.hdr" -o $ASSETS_DIR/hdri/industrial.hdr
+echo "Downloading industrial HDRI..."
+curl -L "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/industrial_sunset_puresky_2k.hdr" -o $ASSETS_DIR/hdri/industrial.hdr
 
 echo "Checking downloads..."
 ls -lh $ASSETS_DIR/textures/concrete/
